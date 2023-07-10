93d9916ae9deb02982d8a381720ace0d1e095a46
Robert Gevorgyan <robert1999.g@gmail.com>
2023-07-10 14:45:46 +0400

    Add support for records

--- pattern-match.ts
+++ pattern-match.ts
@@ -1,36 +1,33 @@
-import { win32 } from "path";
-
 type Pattern<T = any> = { _patternType: string, _data?: T };
-type Matchable = number | string | boolean | Matchable[] | TupleVal;
-type TupleVal = {_valueType: string, _data: Matchable[]}
-type PatternAndCb = [Pattern, (...args: any) => any]
+interface MatchableRecord extends Record<string, Matchable> {};
+type Matchable = number | string | boolean | Matchable[] | TupleVal | MatchableRecord;
+type TupleVal = {_valueType: 'tuple', _data: Matchable[]};
+type PatternAndCb = [Pattern, (...args: any) => any];
 
-const Const: <T>(a: T) => Pattern<T> = (a) => {
-  return {
-    _patternType: 'ConstP',
-    _data: a
-  }
-}
+const Const: <T>(a: T) => Pattern<T> = (a) => ({
+  _patternType: 'ConstP',
+  _data: a
+})
 
-const Variable: (a: string) => Pattern = () => {
-  return {
-    _patternType: 'VariableP',
-  }
-}
+const Variable: (a: string) => Pattern = () => ({
+  _patternType: 'VariableP',
+})
 
-const Cons: (...args: string[]) => Pattern<string[]> = (...args) => {
-  return {
-    _patternType: 'ConsP',
-    _data: args
-  }
-}
+const Cons: (...args: string[]) => Pattern<string[]> = (...args) => ({
+  _patternType: 'ConsP',
+  _data: args
+})
 
-const Tuple: (...args: Pattern[]) => Pattern<Pattern[]> = (...args) => {
-  return {
-    _patternType: 'TupleP',
-    _data: args,
-  }
-}
+const Tuple: (...args: Pattern[]) => Pattern<Pattern[]> = (...args) => ({
+  _patternType: 'TupleP',
+  _data: args,
+})
+
+const Record: (a: Record<string, Pattern>) => Pattern<Record<string, Pattern>> = 
+  (a) => ({
+    _patternType: 'RecordP',
+    _data: a,
+  })
 
 const MakeTuple: (...args: Matchable[]) => Matchable = (...args) => {
   if (args.length <= 1) {
@@ -51,8 +48,9 @@ const _: Pattern = {
 // TODO: const Record
 
 const determineValueAndType = (value: Matchable) => {
-  if (typeof value === 'object' && !Array.isArray(value) && value._valueType) {
-    return value
+  if (typeof value === 'object' && !Array.isArray(value) && value._valueType === 'tuple') {
+    // Is for sure tuple 
+    return value as TupleVal
   }
 
   return {
@@ -90,7 +88,7 @@ const matches: (value: Matchable, pattern: Pattern) => Matchable[] | null = (val
         // from the beginning plus one more argument that's gonna be the
         // tail aka the rest of the elements. So check if there are enough 
         // elements in the value to match the pattern
-        if (patternType === 'ConsP'){
+        if (patternType === 'ConsP') {
           const patternData = (pattern as Pattern<string[]>)._data;
           const length = patternData ? patternData.length : -1;
           if (data.length <= 1 && length <= 1 && data.length === length) {
@@ -101,6 +99,24 @@ const matches: (value: Matchable, pattern: Pattern) => Matchable[] | null = (val
             return [...data.slice(0, length - 1), data.slice(length - 1)]
           }
         } 
+      } else {
+        if (patternType === 'RecordP') {
+          const patternData = (pattern as Pattern<Record<string, Pattern>>)._data || {};
+          return Object.keys(patternData).reduce(
+            (acc: Matchable[] | null, patternKey) => {
+              const patternComponent = patternData[patternKey];
+              const valueComponent = (data as MatchableRecord)[patternKey];
+              if (acc !== null && data.hasOwnProperty(patternKey)) {
+                const newBindings = matches(valueComponent, patternComponent);
+                if (newBindings !== null) {
+                  return acc.concat(newBindings);
+                }
+              }
+
+              return null;
+            }, []
+          );
+        }
       }
       return null
     case 'tuple':
@@ -194,10 +210,20 @@ match(MakeTuple(a, [4,5,6]))._with(
   [Tuple(Cons('ha', 'ta'), Cons('hb', 'tb')), (ha, ta, hb, tb) => console.log(ha, ta, hb, tb)] 
 )
 
-const square_funny = (x: number) =>
-    match(x)._with(
-        [Variable('y'), (y) => y * y]
-    )
+// All keys exist exactly as in the value
+// Match values of each key for a match
+// OR
+// Value has all the keys mentioned by the pattern 
+// (and maybe some other keys) 
+// MAYBE: Change the logic a bit to make it more TypeScript-y
+match({a: 1, b: 2})._with(
+  [Record({a: Variable('x'), b: _}), (x) => console.log(x)]
+)
+
+match({a: 1, b: 2})._with(
+  [Record({a: Const(2), b: _}), (x) => console.log(x)],
+  [Record({a: Const(1), b: Variable('x')}), (x) => console.log(x)]
+)
 
 const non_decreasing: (x: number[]) => boolean = (x) => 
   match(x)._with(
